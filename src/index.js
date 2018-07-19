e1484703e8c76ff1985efe8cd422e1ef04c541e8
vlewisGH <vincent.sean.lewis@gmail.com>
2018-07-19 10:25:32 -0500

    created loading message, posted movies to dom, and ability to add movies(refresh,reload,and changing to action page needs to be disabled)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /**
  * es6 modules and imports
  */
+const $ = require('jquery');
+
 import sayHello from './hello';
 sayHello('World');
 
@@ -9,12 +11,21 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+const displayMovies = () => {
+    let moviesAry = [];
+    $('#movies').html('Loading...');
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        console.log(typeof movies);
+        movies.forEach(({title, rating, id}) => {
+            moviesAry.push(`<h3>id#${id} - ${title} - rating: ${rating}</h3>`);
+        });
+        $('#movies').html(moviesAry);
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+};
+// $('form').submit(()=> event.preventDefault());
+displayMovies();
+
