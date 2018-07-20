786959b5f4aa554e204ce62e442fe0dc16efe5ae
vlewisGH <vincent.sean.lewis@gmail.com>
2018-07-20 15:42:59 -0500

    delete fixed

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
-module.export = {
-    deleteMovie : number => {
+module.exports = {
+    deleteMovie: number => {
 
         const url = `/api/movies/${number}`;
         const options = {
@@ -16,4 +16,23 @@ module.export = {
                 console.log(error);
             })
     },
+    editMovie : () => {
+        const newMovies = {title: $('#title').val(), rating: $('#rating').val()};
+        console.log(newMovies);
+        const url = '/api/movies';
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovies),
+        }
+        fetch(url, options)
+            .then(response => response.json())
+            .catch((error) => {
+                alert('Oh no! Something went wrong.\nCheck the console for details.')
+                console.log(error);
+            })
+    }
+
 };
